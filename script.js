6748551d0639387aae568944e661a21023c74863
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2023-10-15 11:21:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Aerosmith'];
-
-// Function to remove articles and format band names
-function formatBandName(bandName) {
-  const articles = ['the', 'a', 'an'];
-  let words = bandName.toLowerCase().split(' ');
-  
-  // Remove articles from the beginning of the name
-  while (articles.includes(words[0])) {
-    words.shift();
-  }
-  
-  return words.join(' ');
+let arr = ['The Virupaksha Temple', 'A Victoria Memorial', 'An Taj Mahal'];
+let withoutArticles = [];
+let map = {};
+for(let i = 0; i < arr.length; i++) {
+	let str = arr[i];
+	str = str.replace(/\ban\b|\bthe\b|\ba\b/gi, "")
+	str = str.replace(/ +/gi, " ");
+	str = str.trim();
+	map[str] = arr[i];
+	withoutArticles.push(str);
 }
-
-// Sort the band names
-bandNames.sort((a, b) => formatBandName(a) > formatBandName(b) ? 1 : -1);
-
-// Get the ul element by id
-let ulElement = document.getElementById('band');
-
-// Loop through sorted band names and create li elements
-for (let bandName of bandNames) {
-  let liElement = document.createElement('li');
-  liElement.textContent = bandName;
-  ulElement.appendChild(liElement);
+console.log(withoutArticles);
+withoutArticles.sort();
+for(let i = 0; i < withoutArticles.length; i++) {
+	console.log(map[ withoutArticles[i] ]);
 }
